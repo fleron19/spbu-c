48b520c82e0a40f0b7776af0e16164d0fb6010f5
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-09-22 12:06:41 +0300

    Add substring search

---
+++ src/comp-proc/substring_search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int substringCount(char *str, char *substr)
+{
+    int counter = 0;
+    int indstr = 0;
+    int indsubstr = 0;
+    while (str[indstr] != '\0') {
+        if (str[indstr] == substr[indsubstr]){
+            indsubstr ++;
+        }
+        else{
+            indsubstr = 0;
+        }
+        if (substr[indsubstr] == '\0'){
+            counter += 1;
+            indsubstr = 0;
+        }
+        indstr++;
+    }
+    return counter;
+}
+
+
+int main()
+{
+    printf("%d\n", substringCount("testtest12test", "test"));
+    printf("%d\n", substringCount("testtest12test", "tt"));
+    return 0;
+}
