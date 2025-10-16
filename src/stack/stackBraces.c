0dda7e32d01105e2c4405a01731b049e36d3fdc9
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-10-16 17:25:22 +0300

    Add stack-using braces validation algorithm

---
+++ src/stack/stackBraces.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "stack.h"
+
+bool isValid(char* str)
+{
+    Stack stack = newStack();
+    for (int i = 0; i < strlen(str); i ++) {
+        if ((str[i] == '(') || (str[i] == '[') || (str[i] == '{')) {
+            push(&stack, str[i]);
+        }
+        else if ((str[i] == ')') || (str[i] == ']') || (str[i] == '}')) {
+            if (stack.head != NULL) {
+                char last = pop(&stack);
+                if (((str[i] == ')') && (last != '(')) || ((str[i] == '}') && (last != '{')) || ((str[i] == ']') && (last != '['))){
+                    return false;    
+                }
+            }
+            else {
+                return false;
+            }
+        }    
+    }
+    return stack.head == NULL;
+}
+int main(void)
+{   
+    char input[100];
+    scanf("%[^\n]", input);
+    bool res = isValid(input);
+    if (res) {
+        printf("%s", "Valid!");
+    }
+    else {
+        printf("%s", "Not valid!");
+    }
+    
+    printf("\n");
+
+    return 0;
+}
