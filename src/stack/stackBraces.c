bf53c4e92412963dfac53456f2f256fe8c7d01f0
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-10-20 17:04:19 +0300

    Fix styling

--- src/stack/stackBraces.c
+++ src/stack/stackBraces.c
@@ -1,42 +1,39 @@
+#include "stack.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include "stack.h"
 
 bool isValid(char* str)
 {
     Stack* stack = newStack();
-    for (int i = 0; i < strlen(str); i ++) {
+    for (int i = 0; i < strlen(str); i++) {
         if ((str[i] == '(') || (str[i] == '[') || (str[i] == '{')) {
             push(stack, str[i]);
-        }
-        else if ((str[i] == ')') || (str[i] == ']') || (str[i] == '}')) {
+        } else if ((str[i] == ')') || (str[i] == ']') || (str[i] == '}')) {
             if (!isEmpty(stack)) {
                 char last = pop(stack);
-                if (((str[i] == ')') && (last != '(')) || ((str[i] == '}') && (last != '{')) || ((str[i] == ']') && (last != '['))){
-                    return false;    
+                if (((str[i] == ')') && (last != '(')) || ((str[i] == '}') && (last != '{')) || ((str[i] == ']') && (last != '['))) {
+                    return false;
                 }
-            }
-            else {
+            } else {
                 return false;
             }
-        }    
+        }
     }
     return isEmpty(stack);
 }
 int main(void)
-{   
+{
     char input[100];
     scanf("%[^\n]", input);
     bool res = isValid(input);
     if (res) {
         printf("%s", "Valid!");
-    }
-    else {
+    } else {
         printf("%s", "Not valid!");
     }
-    
+
     printf("\n");
 
     return 0;
