6cd8004bda999004b93044c1fa80e6a624844cb3
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-12-11 18:02:02 +0300

    Add task3

---
+++ src/test11.12/task3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "linked_list.h"
+
+int main()
+{
+    struct LinkedList* list = newLinkedList();
+    char inp[64] = "";
+    char* endptr;
+    printf("Введите число (q для выхода): ");
+    scanf(" %s", inp);
+    while (strcmp(inp, "q") != 0) {
+        int value = strtol(inp, &endptr, 10);
+        pushBack(list, value);
+        printf("Введите число (q для выхода): ");
+        scanf(" %s", inp);
+    }
+    printf("Исходный список\n");
+    printList(list);
+
+    struct LinkedList* reversed = reverse(list); // возвращаем указатель на новый перевернутый список
+    printf("\nПеревернутый \n");
+    printList(reversed);
+
+    clearList(list);
+
+    return 0;
+}
