2ba464307093c2c82f5e4c721e5369004728e923
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-12-21 16:49:56 +0300

    Delete aligned, change pop

---
+++ src/stack/stack.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdbool.h>
+
+typedef struct Stack Stack;
+
+Stack* newStack(void);
+void push(Stack* stack, char value);
+// can pop only from non-empty stack, checking it with isEmpty before opping is necessary
+char pop(Stack* stack);
+bool isEmpty(Stack* stack);
+// every created stack should be eventually deleted using this function to prevent memory leakage
+void deleteStack(Stack* stack);
