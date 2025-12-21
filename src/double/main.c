40ad9bd54f6c36d4ee55492c9d4f62583924c1de
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-12-21 20:53:37 +0300

    Minor fixes

--- src/double/main.c
+++ src/double/main.c
@@ -4,7 +4,6 @@
 typedef union {
     double d;
     uint64_t u;
-<<<<<<< HEAD
     struct {
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
         uint64_t frac : 52;
@@ -16,28 +15,15 @@ typedef union {
         uint64_t frac : 52;
 #endif
     } parts;
-} double_bits;
-=======
-    unsigned char bytes[8];
-} DoubleBits;
->>>>>>> 5ce1d6be535301bc8623446422389c1cd507b769
+} doubleBits;
 
 void printSm2p(double x)
 {
-    double_bits db;
-<<<<<<< HEAD
+    doubleBits db;
     db.d = x;
     int sign = db.parts.sign;
     uint64_t exp_bits = db.parts.exp;
     uint64_t frac = db.parts.frac;
-=======
-    memcpy(db.bytes, &x, sizeof db.bytes);
-    uint64_t u = db.u;
-
-    int sign = (u >> 63) & 1;
-    uint64_t expBits = (u >> 52) & 0x7FFULL;
-    uint64_t frac = u & ((1ULL << 52) - 1);
->>>>>>> 5ce1d6be535301bc8623446422389c1cd507b769
     const int bias = 1023;
 
     if (exp_bits == 0) {
@@ -54,6 +40,7 @@ int main(void)
 {
     double x;
     printf("Enter a number: ");
-    if (scanf("%lf", &x) == 1) prinSm2p(x);
+    scanf("%lf", &x);
+    printSm2p(x);
     return 0;
 }
